38898044afa86487138cf50ecf7352c573d96dd8
August <augustt198@gmail.com>
2017-07-23 09:23:44 -0400

    Dab on the haters

---
+++ script.rb
@@ -0,0 +1,810 @@
+CHAR_A = "
+_XXX_
+X___X
+X___X
+XXXXX
+X___X
+X___X
+X___X
+X___X"
+
+CHAR_B = "
+XXXX_
+X___X
+X___X
+XXXX_
+X___X
+X___X
+X___X
+XXXX_"
+
+CHAR_C = "
+_XXX_
+X___X
+X____
+X____
+X____
+X____
+X___X
+_XXX_"
+
+CHAR_D = "
+XXXX_
+X___X
+X___X
+X___X
+X___X
+X___X
+X___X
+XXXX_"
+
+CHAR_E = "
+XXXXX
+X____
+X____
+X____
+XXXXX
+X____
+X____
+XXXXX"
+CHAR_F = "
+XXXXX
+X____
+X____
+XXXXX
+X____
+X____
+X____
+X____"
+
+CHAR_G = "
+_XXX_
+X___X
+X____
+X____
+X__XX
+X___X
+X___X
+_XXX_"
+
+CHAR_H = "
+X___X
+X___X
+X___X
+XXXXX
+X___X
+X___X
+X___X
+X___X"
+
+CHAR_I = "
+XXXXX
+__X__
+__X__
+__X__
+__X__
+__X__
+__X__
+XXXXX"
+
+CHAR_J = "
+XXXXX
+__X__
+__X__
+__X__
+__X__
+__X__
+X_X__
+_X___"
+
+CHAR_K = "
+X___X
+X__X_
+X_X__
+XX___
+X_X__
+X__X_
+X___X
+X___X"
+
+CHAR_L = "
+X____
+X____
+X____
+X____
+X____
+X____
+X____
+XXXXX"
+
+CHAR_M = "
+_X_X_
+X_X_X
+X_X_X
+X_X_X
+X___X
+X___X
+X___X
+X___X"
+
+CHAR_N = "
+X___X
+XX__X
+XX__X
+X_X_X
+X_X_X
+X__XX
+X__XX
+X___X"
+
+CHAR_O = "
+_XXX_
+X___X
+X___X
+X___X
+X___X
+X___X
+X___X
+_XXX_"
+
+CHAR_P = "
+_XXX_
+X___X
+X___X
+XXXX_
+X____
+X____
+X____
+X____"
+
+CHAR_Q = "
+_XXX_
+X___X
+X___X
+X___X
+X___X
+X_X_X
+_XXX_
+___X_"
+
+CHAR_R = "
+_XXX_
+X___X
+X___X
+XXXX_
+X__X_
+X___X
+X___X
+X___X"
+
+CHAR_S = "
+_XXX_
+X___X
+X____
+_XXX_
+____X
+____X
+X___X
+_XXX_"
+
+CHAR_T = "
+XXXXX
+__X__
+__X__
+__X__
+__X__
+__X__
+__X__
+__X__"
+
+CHAR_U = "
+X___X
+X___X
+X___X
+X___X
+X___X
+X___X
+X___X
+_XXX_"
+
+CHAR_V = "
+X___X
+X___X
+X___X
+_X_X_
+_X_X_
+_X_X_
+__X__
+__X__"
+
+CHAR_W = "
+X___X
+X___X
+X___X
+X___X
+X_X_X
+X_X_X
+X_X_X
+_X_X_"
+
+CHAR_X = "
+X___X
+X___X
+X___X
+_X_X_
+__X__
+_X_X_
+X___X
+X___X"
+
+CHAR_Y = "
+X___X
+X___X
+X___X
+_X_X_
+__X__
+__X__
+__X__
+__X__"
+
+CHAR_Z = "
+XXXXX
+____X
+____X
+___X_
+__X__
+_X___
+X____
+XXXXX"
+
+CHAR_a = "
+_____
+_____
+_____
+_XXX_
+____X
+_XXXX
+X___X
+_XXXX"
+
+CHAR_b = "
+_____
+X____
+X____
+X____
+XXXX_
+X___X
+X___X
+XXXX_"
+
+CHAR_c = "
+_____
+_____
+_____
+_XXX_
+X___X
+X____
+X___X
+_XXX_"
+
+CHAR_d = "
+_____
+____X
+____X
+____X
+_XXXX
+X___X
+X___X
+_XXXX"
+
+CHAR_e = "
+_____
+_____
+_____
+_XXX_
+X___X
+XXXXX
+X____
+_XXX_"
+
+CHAR_f = "
+_____
+__XX_
+_X__X
+_X___
+XXXX_
+_X___
+_X___
+_X___"
+
+CHAR_g = "
+_____
+_____
+_____
+_XXX_
+X___X
+_XXXX
+____X
+_XXX_"
+
+CHAR_h = "
+_____
+X____
+X____
+X____
+XXXX_
+X___X
+X___X
+X___X"
+
+CHAR_i = "
+___
+___
+___
+_X_
+___
+_X_
+_X_
+_X_"
+
+CHAR_j = "
+____
+____
+____
+__X_
+____
+__X_
+X_X_
+_X__"
+
+CHAR_k = "
+_____
+X____
+X__X_
+X_X__
+XX___
+X_X__
+X__X_
+X__X_"
+
+CHAR_l = "
+___
+_X_
+_X_
+_X_
+_X_
+_X_
+_X_
+_X_"
+
+CHAR_m = "
+______
+______
+______
+X_X_X_
+XX_X_X
+X__X_X
+X____X
+X____X"
+
+CHAR_n = "
+_____
+_____
+_____
+X_XX_
+XX__X
+X___X
+X___X
+X___X"
+
+CHAR_o = "
+_____
+_____
+_____
+_XXX_
+X___X
+X___X
+X___X
+_XXX_"
+
+CHAR_p = "
+_____
+_____
+_____
+X_XX_
+XX__X
+X___X
+XXXX_
+X____"
+
+CHAR_q = "
+_____
+_____
+_____
+_XX_X
+X__XX
+X___X
+_XXXX
+____X"
+
+CHAR_r = "
+_____
+_____
+_____
+_____
+X_XX_
+XX__X
+X____
+X____"
+
+CHAR_s = "
+_____
+_____
+_____
+_XXX_
+X____
+_XXX_
+____X
+_XXX_"
+
+CHAR_t = "
+_____
+_____
+_X___
+XXXX_
+_X___
+_X___
+_X___
+__XX_"
+
+CHAR_u = "
+_____
+_____
+_____
+_____
+X___X
+X___X
+X__XX
+_XX_X"
+
+CHAR_v = "
+_____
+_____
+_____
+_____
+X___X
+_X_X_
+_X_X_
+__X__"
+
+CHAR_w = "
+______
+______
+______
+X____X
+X____X
+X____X
+X_XX_X
+_X__X_"
+
+CHAR_x = "
+_____
+_____
+_____
+X___X
+_X_X_
+__X__
+_X_X_
+X___X"
+
+CHAR_y = "
+_____
+_____
+_____
+X___X
+X___X
+_XXXX
+____X
+_XXX_"
+
+CHAR_z = "
+_____
+_____
+_____
+XXXXX
+___X_
+__X__
+_X___
+XXXXX"
+
+CHAR_1 = "
+__X__
+_XX__
+X_X__
+__X__
+__X__
+__X__
+__X__
+XXXXX"
+
+CHAR_2 = "
+_XXX_
+X___X
+____X
+___X_
+__X__
+_X___
+X____
+XXXXX"
+
+CHAR_3 = "
+_XXX_
+X___X
+____X
+___X_
+____X
+____X
+X___X
+_XXX_"
+
+CHAR_4 = "
+X__X_
+X__X_
+X__X_
+XXXXX
+___X_
+___X_
+___X_
+___X_"
+
+CHAR_5 = "
+XXXXX
+X____
+X____
+XXX__
+___X_
+____X
+X___X
+_XXX_"
+
+CHAR_6 = "
+_XXX_
+X___X
+X____
+X____
+XXXX_
+X___X
+X___X
+_XXX_"
+
+CHAR_7 = "
+XXXXX
+____X
+____X
+___X_
+__X__
+_X___
+_X___
+_X___"
+
+CHAR_8 = "
+_XXX_
+X___X
+X___X
+_XXX_
+X___X
+X___X
+X___X
+_XXX_"
+
+CHAR_9 = "
+_XXX_
+X___X
+X___X
+X___X
+_XXXX
+____X
+____X
+____X"
+
+CHAR_0 = "
+_XXX_
+X__XX
+X__XX
+X_X_X
+X_X_X
+XX__X
+XX__X
+_XXX_"
+
+CHAR_QUES = "
+_XXX_
+X___X
+____X
+___X_
+__X__
+__X__
+_____
+__X__"
+
+CHAR_BANG = "
+__X__
+__X__
+__X__
+__X__
+__X__
+__X__
+_____
+__X__"
+
+CHAR_MINUS = "
+_____
+_____
+_____
+_____
+XXXXX
+_____
+_____
+_____"
+
+CHAR_PLUS = "
+_____
+_____
+__X__
+__X__
+XXXXX
+__X__
+__X__
+_____"
+
+CHAR_STAR = "
+_____
+X_X_X
+_XXX_
+XXXXX
+_XXX_
+X_X_X
+_____
+_____"
+
+CHAR_LPAREN = "
+____X
+___X_
+___X_
+__X__
+__X__
+__X__
+___X_
+____X"
+
+CHAR_GT = "
+_____
+_____
+_X___
+__X__
+___X_
+__X__
+_X___
+_____"
+
+CHAR_LT = "
+_____
+_____
+___X_
+__X__
+_X___
+__X__
+___X_
+_____"
+
+CHAR_DOT = "
+___
+___
+___
+___
+___
+___
+___
+_X_"
+
+CHAR_COMMA = "
+____
+____
+____
+____
+____
+____
+__X_
+_X__"
+
+
+
+require 'sinatra'
+require 'httparty'
+
+MAP = {
+    'A' => CHAR_A, 'B' => CHAR_B, 'C' => CHAR_C, 'D' => CHAR_D, 'E' => CHAR_E, 'F' => CHAR_F,
+    'G' => CHAR_G, 'H' => CHAR_H, 'I' => CHAR_I, 'J' => CHAR_J, 'K' => CHAR_K, 'L' => CHAR_L,
+    'M' => CHAR_M, 'N' => CHAR_N, 'O' => CHAR_O, 'P' => CHAR_P, 'Q' => CHAR_Q, 'R' => CHAR_R, 'S' => CHAR_S,
+    'T' => CHAR_T, 'U' => CHAR_U, 'V' => CHAR_V, 'W' => CHAR_W, 'X' => CHAR_X, 'Y' => CHAR_Y,
+    'Z' => CHAR_Z, '1' => CHAR_1, '2' => CHAR_2, '3' => CHAR_3, '4' => CHAR_4, '5' => CHAR_5,
+    '6' => CHAR_6, '7' => CHAR_7, '8' => CHAR_8, '9' => CHAR_9, '?' => CHAR_QUES, '!' => CHAR_BANG,
+    '0' => CHAR_0,
+    '-' => CHAR_MINUS, '+' => CHAR_PLUS, '*' => CHAR_STAR, '(' => CHAR_LPAREN,
+    'a' => CHAR_a, 'b' => CHAR_b, 'c' => CHAR_c, 'd' => CHAR_d, 'e' => CHAR_e, 'f' => CHAR_f,
+    'g' => CHAR_g, 'h' => CHAR_h, 'i' => CHAR_i, 'j' => CHAR_j, 'k' => CHAR_K, 'l' => CHAR_l,
+    'm' => CHAR_m, 'n' => CHAR_n, 'o' => CHAR_o, 'p' => CHAR_p, 'q' => CHAR_q, 'r' => CHAR_r,
+    's' => CHAR_s, 't' => CHAR_t, 'u' => CHAR_u, 'v' => CHAR_v, 'w' => CHAR_w, 'x' => CHAR_x,
+    'y' => CHAR_y, 'z' => CHAR_z, '>' => CHAR_GT, '<' => CHAR_LT, '.' => CHAR_DOT, ',' => CHAR_COMMA
+}
+
+def ascii(text, replaces)
+    lines = Array.new(8, '')
+    text.chars.each do |c|
+        #c = c.downcase
+        if c == ' '
+            lines.each { |s| s.concat('          ') }
+        elsif MAP.keys.include? c
+            puts '__'
+            ltr = MAP[c][1..-1].split("\n")
+            ltr.each_with_index do |l, idx|
+		_i = 0
+                l = l.chars.map do |chr|
+                    puts chr
+                    if chr == '_'
+                        n = ':d:'
+                    elsif chr == 'X'
+                        n = replaces[_i % replaces.length]
+                    end
+                    _i += 1
+                    n
+                end.join
+                puts l
+                # l = l.gsub('_', ':d:').gsub('X') do |ignore|
+                #     n = replaces[_i % replaces.length]
+                #     _i += 1
+                #     n
+                # end
+                lines[idx] += l + '    '
+            end
+        end
+    end
+    return lines.join("\n")
+end
+
+get '/emoji' do
+    status 200
+    text = params[:text]
+    # haha no
+    if params[:user_name].downcase.include?("forest")
+        return ':timetostop: :parrot1: :explody_parrot: :sonic: ' * 15000
+    end
+    if text.split("/").length != 2
+        return "Bad format, should be: content / emoji"
+    else
+        t, r = text.split("/")
+        r = r.split("~").map { |x| x.strip }
+        if params[:user_name].downcase.include?("forest")
+            r = [":middle_finger:"]
+        end
+        str = ascii(t, r)
+        j = {
+            "text" => str,
+            "response_type" => "in_channel"
+        }
+        content_type 'application/json'
+        return j.to_json
+    end
+end
+
+def process(text, channel)
+    content = ""
+    if text.split("/").length != 2
+        content = "Bad format"
+    else
+        t, r = text.split("/")
+        content = ascii(t, r)
+    end
+
+    HTTParty.post HOOKURL, body: {"text" => content, "username" => "ASCII Art", "channel" => channel}.to_json, headers: {'content-type' => 'application/json'}
+end
